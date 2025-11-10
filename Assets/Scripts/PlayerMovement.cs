d7196e135733307d939f2435c080f1dd1f4ce7de
Kaelan Bartlett <228972983+Helodity-M@users.noreply.github.com>
2025-11-10 12:55:09 -0500

    Viewport Boundaries

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,7 @@ public class PlayerMovement : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     InputAction moveAction;
     [SerializeField] float MoveSpeed;
+    [SerializeField] float boundsPadding;
     void Start()
     {
         moveAction = InputSystem.actions.FindAction("Move");   
@@ -15,5 +16,22 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         transform.position += (Vector3)moveAction.ReadValue<Vector2>() * Time.deltaTime * MoveSpeed;
+
+
+
+        ConstrainPlayer();
+    }
+
+    void ConstrainPlayer()
+    {
+        Vector2 minBounds = Camera.main.ViewportToWorldPoint(Vector2.zero);
+        Vector2 maxBounds = Camera.main.ViewportToWorldPoint(Vector2.one);
+
+        transform.position =
+            new Vector2(
+                Mathf.Clamp(transform.position.x, minBounds.x + boundsPadding, maxBounds.x - boundsPadding),
+                Mathf.Clamp(transform.position.y, minBounds.y + boundsPadding, maxBounds.y - boundsPadding)
+                );
+
     }
 }
