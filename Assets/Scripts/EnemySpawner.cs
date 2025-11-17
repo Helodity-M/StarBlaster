0d663c8afa33d7f99437546f5ca8bae529332897
Kaelan Bartlett <228972983+Helodity-M@users.noreply.github.com>
2025-11-17 11:14:18 -0500

    While Loops

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -6,6 +6,7 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField] WaveConfigSO[] waveList;
     [SerializeField] int currentWaveIndex;
     [SerializeField] float timeBetweenWaveSpawns;
+    [SerializeField] bool isLooping;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,19 +17,22 @@ public class EnemySpawner : MonoBehaviour
 
     IEnumerator SpawnEnemies()
     {
-        foreach (WaveConfigSO wave in waveList)
+        while(isLooping)
         {
-            for (int i = 0; i < wave.GetEnemyCount(); i++)
+            foreach (WaveConfigSO wave in waveList)
             {
-                Pathfinding newPath = Instantiate(
-                    wave.GetEnemyPrefab(0),
-                    wave.GetStartingWaypoint().position,
-                    Quaternion.identity
-                );
-                newPath.SetWaveConfig(wave);
-                yield return new WaitForSeconds(wave.GetRandomSpawnDelay());
+                for (int i = 0; i < wave.GetEnemyCount(); i++)
+                {
+                    Pathfinding newPath = Instantiate(
+                        wave.GetEnemyPrefab(0),
+                        wave.GetStartingWaypoint().position,
+                        Quaternion.identity
+                    );
+                    newPath.SetWaveConfig(wave);
+                    yield return new WaitForSeconds(wave.GetRandomSpawnDelay());
+                }
+                yield return new WaitForSeconds(timeBetweenWaveSpawns);
             }
-            yield return new WaitForSeconds(timeBetweenWaveSpawns);
         }
     }
     public WaveConfigSO GetCurrentWave()
