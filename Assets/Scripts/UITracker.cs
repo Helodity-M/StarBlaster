6ab1c5f4bcffb33f3951ff0dadba1063a354b7b4
Kaelan Bartlett <228972983+Helodity-M@users.noreply.github.com>
2025-11-23 12:32:34 -0500

    Added game UI

---
+++ Assets/Scripts/UITracker.cs
@@ -0,0 +1,26 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UITracker : MonoBehaviour
+{
+    [Header("Health")]
+    [SerializeField] Health playerHealth;
+    [SerializeField] Slider HealthUI;
+
+    [Header("Score")]
+    [SerializeField] ScoreTracker scoreTracker;
+    [SerializeField] TMP_Text ScoreUI;
+
+    private void Awake()
+    {
+        HealthUI.maxValue = playerHealth.health;
+    }
+    private void Update()
+    {
+        HealthUI.value = playerHealth.health;
+        ScoreUI.text = $"Score:\n{scoreTracker.score}";
+    }
+
+
+}
